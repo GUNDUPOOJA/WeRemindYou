ba6287e77090005416fdc8bf8ec0339f89a628b0
Teju2404 <kandulatejaswireddy@gmail.com>
2020-10-11 00:09:38 -0500

    added rateUsActivity.java and rateus.xml files

---
+++ app/src/main/java/com/example/weremindyou/rateUsActivity.java
@@ -0,0 +1,19 @@
+package com.example.weremindyou;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class rateUsActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rateus);
+
+
+    }
+
+}
+
