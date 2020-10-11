c360319952d951adb57051e9ed8f682c0452a8d4
Teju2404 <kandulatejaswireddy@gmail.com>
2020-10-11 00:08:13 -0500

    Updated Main activity

--- app/src/main/java/com/example/weremindyou/MainActivity.java
+++ app/src/main/java/com/example/weremindyou/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,16 +22,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         createTask = findViewById(R.id.taskBtn);
+
         createTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Log.d(g,"testing the app");
-               Intent editIntent = new Intent(MainActivity.this, EditActivity.class);
+                Intent editIntent = new Intent(MainActivity.this, EditActivity.class);
                 startActivity(editIntent);
-                setContentView(R.layout.activity_edit);
+                // setContentView(R.layout.activity_edit);
             }
-
-
         });
+
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.help:
+                Intent helpint = new Intent(MainActivity.this, HelpFragment.class);
+                startActivity(helpint);
+                return true;
+            case R.id.dashboard:
+                Intent boardint = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(boardint);
+                return true;
+            case R.id.rateUs:
+                Intent rateint= new Intent(MainActivity.this, rateUsActivity.class);
+                startActivity(rateint);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    }
+
