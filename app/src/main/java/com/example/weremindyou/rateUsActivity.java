c7bb0bf8a6299f6ace1ff09b1497bda6d1fd18c4
nrajubn <60019513+nrajubn@users.noreply.github.com>
2020-10-30 17:43:31 -0500

    I have added the activity for rate us where i have created the method using toast and rating bar widget.

--- app/src/main/java/com/example/weremindyou/rateUsActivity.java
+++ app/src/main/java/com/example/weremindyou/rateUsActivity.java
@@ -1,17 +1,25 @@
 package com.example.weremindyou;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class rateUsActivity extends AppCompatActivity{
-
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rateus);
-
+        ratingBar = findViewById(R.id.ratingbar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(rateUsActivity.this, "Rating: "+v, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
