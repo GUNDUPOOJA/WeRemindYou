6b8c8461b4b3defb6a0fc6bae5bf2faeffa369aa
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2020-10-30 10:13:59 -0500

    working on time button

--- app/src/main/java/com/example/weremindyou/EditActivity.java
+++ app/src/main/java/com/example/weremindyou/EditActivity.java
@@ -27,28 +27,16 @@ import java.util.GregorianCalendar;
 
 public class EditActivity extends AppCompatActivity {
 
-
-    private Task task;
-    private GregorianCalendar calendar;
-    private int year;
-    private int month;
-    private int day;
-    private int hour;
-    private int minute;
     RadioGroup radioGroup;
     RadioButton selectedPriority;
     EditText editText_Name;
     boolean isEnabled ;
     String name;
-    private Button dateButton;
-    private Button timeButton;
+    Calendar calendar;
+    Button dateButton;
+    Button timeButton;
     Button btn_done;
-
-    static final int DATE_DIALOG_ID = 0;
-    static final int TIME_DIALOG_ID = 1;
-
-    private static SimpleDateFormat timeFormat = new SimpleDateFormat("H:mm");
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("E MMM d, yyyy");
+    int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,30 +45,22 @@ public class EditActivity extends AppCompatActivity {
         init();
         gettingValuesFromViews();
 
-
-
-
-    dateButton = (Button)findViewById(R.id.date_button);
-        timeButton = (Button)findViewById(R.id.time_button);
-
-        task = new Task();
-        task.fromIntent(getIntent());
-        calendar = new GregorianCalendar();
-        calendar.setTimeInMillis(task.getDate());
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        hour = calendar.get(Calendar.HOUR_OF_DAY);
-        minute = calendar.get(Calendar.MINUTE);
-
-        updateButtons();
     }
 
     // In this method priority of the remainder i.e high,medium,low is set and also
     //used toast to display a message if nothing selected.
+    // In this the date and time buttons are also created
     private void gettingValuesFromViews() {
 
         radioGroup.getCheckedRadioButtonId();
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar=Calendar.getInstance();
+                mHour = calendar.get(Calendar.HOUR_OF_DAY);
+                mMinute = calendar.get(Calendar.MINUTE);
+            }
+        });
         btn_done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,83 +94,12 @@ public class EditActivity extends AppCompatActivity {
         Intent in = new Intent(this,MainActivity.class);
 //        setResult(RESULT_OK,in);
 
-
         startActivity(in);
       //  setContentView(R.layout.fragment_tasks);
 //        finish();
     }
 
-    @Override
-    protected Dialog onCreateDialog(int id)
-    {
-        if (DATE_DIALOG_ID == id)
-            return new DatePickerDialog(this, dateSetListener, year, month, day);
-        else if (TIME_DIALOG_ID == id)
-            return new TimePickerDialog(this, timeSetListener, hour, minute, true);
-        else
-            return null;
-    }
-    @Override
-    protected void onPrepareDialog(int id, Dialog dialog)
-    {
-        if (DATE_DIALOG_ID == id)
-            ((DatePickerDialog)dialog).updateDate(year, month, day);
-        else if (TIME_DIALOG_ID == id)
-            ((TimePickerDialog)dialog).updateTime(hour, minute);
-    }
-
-    public void onDateClick(View view){
-        showDialog(DATE_DIALOG_ID);
-    }
-
-    public void onTimeClick(View view)
-    {
-        showDialog(TIME_DIALOG_ID);
-    }
 
 
-    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
-    {
-        public void onDateSet(DatePicker view, int year_, int month_, int day_)
-        {
-            year = year_;
-            month = month_;
-            day = day_;
 
-            calendar = new GregorianCalendar(year, month, day, hour, minute);
-            task.setDate(calendar.getTimeInMillis());
-
-            updateButtons();
-        }
-    };
-
-    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener()
-    {
-        public void onTimeSet(TimePicker view, int hour_, int minute_)
-        {
-            hour = hour_;
-            minute = minute_;
-
-            calendar = new GregorianCalendar(year, month, day, hour, minute);
-            task.setDate(calendar.getTimeInMillis());
-
-            updateButtons();
-        }
-    };
-    private void updateButtons()
-    {
-        dateButton.setText(formatDate(task));
-//        Log.d(TAG, "updated date button");
-        timeButton.setText(formatTime(task));
-//        Log.d(TAG, "updated time button");
-    }
-    public static String formatTime(Task Task)
-    {
-        return timeFormat.format(new Date(Task.getDate()));
-    }
-
-    public static String formatDate(Task Task)
-    {
-        return dateFormat.format(new Date(Task.getDate()));
-    }
 }
