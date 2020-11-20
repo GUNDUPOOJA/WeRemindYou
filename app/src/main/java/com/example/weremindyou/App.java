76647799d964bf3aea92be59f55520dfd09fa65b
gundu pooja <gundupooja1995@gmail.com>
2020-11-19 21:50:12 -0600

    created a class called App and added createnotificationchannel method that groups notifications into groups-channels which gives the user the ability to modify notification settings for entire channel all at once.

---
+++ app/src/main/java/com/example/weremindyou/App.java
@@ -0,0 +1,32 @@
+package com.example.weremindyou;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+//  class App that extends Application in which a createNotificationChannel method is added
+
+public class App extends Application {
+
+    public static final String CHANNEL_ID = "ALARM_SERVICE_CHANNEL";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannnel();
+    }
+
+    //Notification channel method is used to group notifications into
+    // groups—channels— so the user has the ability to modify notification settings
+    // for the entire channel at once.
+    private void createNotificationChannnel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Alarm Service Channel",
+                    NotificationManager.IMPORTANCE_NONE
+            );
+        }
+    }
+}
