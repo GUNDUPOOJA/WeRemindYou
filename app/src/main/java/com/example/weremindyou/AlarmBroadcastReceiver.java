791777c72b047074c2a91ebbc1eb782cf25b9609
Teju2404 <kandulatejaswireddy@gmail.com>
2020-11-20 01:31:28 -0600

    Added startAlarmService method in AlarmBroadcastReceiver class.This method is used to set an alarm.

--- app/src/main/java/com/example/weremindyou/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/weremindyou/AlarmBroadcastReceiver.java
@@ -1,23 +1,32 @@
 package com.example.weremindyou;
 
-
-import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
+
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
     public static final String TITLE = "TITLE";
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Broadcast","Received");
+        Log.d("Broadcast", "Received");
         String toastText = String.format("Alarm Reboot");
         Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
-        startAlarmService(context,intent);
+        startAlarmService(context, intent);
     }
-
-
+    private void startAlarmService(Context context, Intent intent) {
+        Intent intentService = new Intent(context, AlarmService.class);
+        intentService.putExtra(TITLE, intent.getStringExtra(TITLE));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intentService);
+        }
+        else {
+            context.startService(intentService);
+        }
+    }
 }
+
