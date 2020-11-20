4b0261fc443aaad3c60a71e9390e8dc5b23042c3
nrajubn <60019513+nrajubn@users.noreply.github.com>
2020-11-19 22:55:24 -0600

    Added a method which will enable the toggle , update alarm and created a arraylist with getAlarmID that will fetch the details of id, alarm_id,title,enabled,priority,hour,minute,date,month,day,location

--- app/src/main/java/com/example/weremindyou/DBHelper.java
+++ app/src/main/java/com/example/weremindyou/DBHelper.java
@@ -45,6 +45,46 @@ public class DBHelper extends SQLiteOpenHelper {
         String query = "UPDATE "+TABLE_NAME +" SET enabled = "+enable_status+" WHERE alaram_id = "+id;
         db.execSQL(query);
     }
+
+    public void updateAlarm(Alarm alarm){
+        db =this.getWritableDatabase();
+        String q = "UPDATE "+ TABLE_NAME+" SET title='"+alarm.getTitle()+ "', enabled = '"+alarm.isEnabled()+"', priority = '"+alarm.getPriority()+"' , hour='"+alarm.getHour()+ "', minute = '"+alarm.getMinute()+"', date = '"+alarm.getDate()+"' , month='"+alarm.getMonth()+ "', day = '"+alarm.getDay()+"', location = '"+alarm.getLocation()+ "' WHERE _id="+alarm.getId();
+
+
+        Log.d("update",q);
+        db.execSQL(q);
+        Log.d("all data","started");
+        getAllData();
+        db.close();
+    }
+    public List<Alarm> getAlarmbyID(int id)
+    {
+
+        List<Alarm> list =new  ArrayList< >() ;
+        db =this.getReadableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME+" WHERE _id = "+id;
+        Cursor result = db.rawQuery(query,null);
+        if (result.moveToFirst()){
+            for (result.moveToFirst(); !result.isAfterLast(); result.moveToNext())
+            {
+                int _id = result.getInt(0);
+                int alarm_id = result.getInt(1);
+                String title = result.getString(2);
+                int enabled=result.getInt(3);
+                String priority = result.getString(4);
+                int hour = result.getInt(5);
+                int minute = result.getInt(6);
+                int date = result.getInt(7);
+                String month = result.getString(8);
+                String day = result.getString(9);
+                String location = result.getString(10);
+                Alarm alarm = new Alarm(_id, alarm_id,title,enabled,priority,hour,minute,date,month,day,location);
+                list.add(alarm);
+            }
+        }
+        db.close();
+        return list;
+    }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_TABLE);
